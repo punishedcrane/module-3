5e62ad4aa8c032fad546843b34b3f151a427b529
punishedcrane <rockmanx62@gmail.com>
2025-03-08 00:46:51 +0530

    fixed some errors

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/module-3" element={<Home />} />
           <Route path="/product/:id" element={<ProductDetails />} />
         </Routes>
       </div>
